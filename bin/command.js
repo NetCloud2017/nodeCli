289df541036a6b52a0e01213d94a1254f42c2d82
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2023-04-13 01:35:16 +0800

    command

--- bin/command.js
+++ bin/command.js
@@ -1,11 +1,16 @@
-// command 库练习
+#!/usr/bin/env node
 
 const { program } = require("commander");
 
 program.name("ccli").description("cli name").version("0.1.0");
 
-program.command("--frist").option("-s, --seperator <char>");
+program.option("--first").option("-s, --separator <char>");
 
 program.parse();
+
 const options = program.opts();
+const limit = options.first ? 1 : undefined;
+// ccli aaa/bbb/ccc --first -s '/' (错误) : 还是用 | 号 吧 这个 / 号在win 系统 会别解析到git安装的根目录；
+//  ccli -s '|' --first "aaa|bbb|ccc" (正确)
+console.log(program.args[0].split(options.separator, limit), program.args); // splite 新用法。
 console.log(options, "command options");
